1966a0db35aa34bff2ecead5a3d27c12e4169f4f
Hyouhyan <hyouhyan@hyouhyan.com>
2024-11-10 17:11:14 +0900

    fix: databaseをuseする行追加

--- init/add_test_record.sql
+++ init/add_test_record.sql
@@ -1,20 +1,22 @@
-INSERT INTO schedules (begin, end, place, content)
-VALUES ('2021-11-13 15:30:00', '2021-11-13 16:00:00', '自宅', '宿題');
+use schedule;
 
 INSERT INTO schedules (begin, end, place, content)
-VALUES ('2021-11-14 10:00:00', '2021-11-14 12:00:00', 'イオン', '買い物');
+VALUES ('2024-11-13 15:30:00', '2024-11-13 16:00:00', '自宅', '宿題');
 
 INSERT INTO schedules (begin, end, place, content)
-VALUES ('2021-11-22 15:30:00', '2021-11-14 17:30:00', 'オンライン', '座談会');
+VALUES ('2024-11-14 10:00:00', '2024-11-14 12:00:00', 'イオン', '買い物');
 
 INSERT INTO schedules (begin, end, place, content)
-VALUES ('2021-11-25 09:00:00', '2021-11-25 12:10:00', '大学', '授業');
+VALUES ('2024-11-22 15:30:00', '2024-11-14 17:30:00', 'オンライン', '座談会');
 
 INSERT INTO schedules (begin, end, place, content)
-VALUES ('2021-11-25 19:00:00', '2021-11-25 20:00:00', 'オンライン', '就活');
+VALUES ('2024-11-25 09:00:00', '2024-11-25 12:10:00', '大学', '授業');
 
 INSERT INTO schedules (begin, end, place, content)
-VALUES ('2021-11-28 10:00:00', '2021-11-28 20:00:00', 'オンライン', 'インターン');
+VALUES ('2024-11-25 19:00:00', '2024-11-25 20:00:00', 'オンライン', '就活');
 
 INSERT INTO schedules (begin, end, place, content)
-VALUES ('2022-01-01 00:00:00', '2022-01-01 23:59:00', '自宅', '正月');
+VALUES ('2024-11-28 10:00:00', '2024-11-28 20:00:00', 'オンライン', 'インターン');
+
+INSERT INTO schedules (begin, end, place, content)
+VALUES ('2025-01-01 00:00:00', '2022-01-01 23:59:00', '自宅', '正月');
